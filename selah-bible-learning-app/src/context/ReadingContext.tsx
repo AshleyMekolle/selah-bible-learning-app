2bd08116cc5c8f1717cb54e03f05d36421619646
Ashley Mekolle <mekolleashleyam@gmail.com>
2025-12-28 14:09:39 +0100

    feat: persist reading streak and completion using AsyncStorage

--- selah-bible-learning-app/src/context/ReadingContext.tsx
+++ selah-bible-learning-app/src/context/ReadingContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState, ReactNode } from "react";
+import { createContext, useContext, useState, ReactNode, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type ReadingContextType ={
     completedToday: boolean;
@@ -7,10 +8,51 @@ type ReadingContextType ={
 };
 
 const ReadingContext = createContext<ReadingContextType | undefined>(undefined);
+const STORAGE_KEYS ={
+    streak: "@reading_streak",
+    completedToday: "@completedToday",
+}
 
 export function ReadingProvider ({children} : {children: ReactNode}){
     const [completedToday, setCompletedToday] = useState(false);
     const [streak, setStreak] = useState(0)
+
+    useEffect(() =>{
+        const loadData = async () =>{
+            try{
+                const storedStreak = await AsyncStorage.getItem(
+                    STORAGE_KEYS.streak
+                );
+                const storedCompleted = await AsyncStorage.getItem(
+                    STORAGE_KEYS.completedToday
+                );
+
+                if (storedStreak !== null){
+                    setStreak(Number(storedStreak));
+                }
+
+                if (storedCompleted !== null){
+                setCompletedToday(storedCompleted === "true");
+                }
+            } catch (error){
+                console.error("Failed to load reading data", error);
+            };
+            }
+
+            loadData();
+        }, []);
+
+        //Persist the user's streak
+
+    useEffect (() =>{
+        AsyncStorage.setItem(STORAGE_KEYS.streak, streak.toString());
+    }, [streak])
+
+    //Persist the user's completion
+
+    useEffect (() =>{
+        AsyncStorage.setItem(STORAGE_KEYS.completedToday, completedToday.toString());
+    }, [completedToday])
 
     const completeReading = () =>{
         if (!completedToday){
