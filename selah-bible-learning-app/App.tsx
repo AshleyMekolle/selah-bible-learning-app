13993e5e67f78798fc23c626b6379772072f3462
Ashley Mekolle <mekolleashleyam@gmail.com>
2025-12-28 14:49:04 +0100

    feat: add typography

--- selah-bible-learning-app/App.tsx
+++ selah-bible-learning-app/App.tsx
@@ -1,9 +1,20 @@
 import AppNavigator from "./src/navigation/AppNavigator";
 import { ReadingProvider } from "./src/context/ReadingContext";
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts, Inter_400Regular, Inter_500Medium, Inter_600SemiBold } from "@expo-google-fonts/inter";
+import { View } from "react-native";
 
 
 export default function App (){
+   const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold
+   })
+
+   if(!fontsLoaded){
+    return <View/>
+   }
   return(
     <ReadingProvider>
       <NavigationContainer>
