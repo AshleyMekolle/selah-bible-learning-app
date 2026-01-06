b907742b72327ad00f5ddd2091ca20838a52616e
Ashley Mekolle <mekolleashleyam@gmail.com>
2026-01-06 09:22:28 +0100

    feat: add init

---
+++ backend/app/api/v1/__init__.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.api.v1.scripture import router as scripture_router
+from app.api.v1.day import router as day_router
+
+router = APIRouter()
+
+router.include_router(scripture_router, prefix="/scripture", tags=["Scripture"])
+router.include_router(day_router, tags=["Day"])
