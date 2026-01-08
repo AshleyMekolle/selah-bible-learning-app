ef8fb10fc400a84c4aa0fe25d7ec170e5a1ae84c
Ashley Mekolle <mekolleashleyam@gmail.com>
2026-01-08 09:59:22 +0100

    feat(progress): add sunday-only weekly reflection

---
+++ my-selah/src/components/weeklyReflection.tsx
@@ -0,0 +1,46 @@
+import { View, Text, StyleSheet } from "react-native";
+import { typography } from "../theme/typography";
+import { colors } from "../theme/color";
+
+type Props = {
+    completedDays: number
+}
+
+export default function weeklyReflection({completedDays}: Props){
+    let message = "Every moment you opened the Word mattered.";
+
+    if(completedDays === 7){
+        message = "You remained faithful this week. Selah and rest.";
+    } else if (completedDays >= 4){
+        message = "You returned often. Grace met you each time.";
+    } else if (completedDays >= 1)
+        message = "You showed up. That is enough to begin again.";
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>Weekly Reflection</Text>
+            <Text style={styles.text}>{message}</Text>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 18,
+        borderRadius: 16,
+        backgroundColor: colors.surface,
+        marginTop: 20
+    },
+    title:{
+        fontSize:16,
+        fontFamily: typography.semibold,
+        marginBottom:8,
+        color: colors.textPrimary
+    },
+    text:{
+        fontSize:14,
+        lineHeight:22,
+        color: colors.textSecondary
+    }
+})
